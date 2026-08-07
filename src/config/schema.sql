c9323f8aef21acc29121d40fdf62abe486cf0170
Coipo-D <liandata@proton.me>
2026-08-06 22:18:40 -0400

    Estandarizacion de base de datos y correcion de variables. Cambio de nombre de tablas a plurales y estandarizar la FK's al sufijo_id

--- src/config/schema.sql
+++ src/config/schema.sql
@@ -1,39 +1,50 @@
 CREATE DATABASE veterinaria_go_prod;
 
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
+-- DROP SCHEMA public CASCADE;
+-- CREATE SCHEMA public;
+-- GRANT ALL ON SCHEMA public TO postgres;
+-- GRANT ALL ON SCHEMA public TO public;
 
-CREATE TABLE person( -- Tabla personas
-	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-	
+CREATE TABLE persons( -- Tabla personas
+	id UUID DEFAULT gen_random_uuid(),
 	-- datos de la personas
 	full_name VARCHAR(100) NOT NULL,
-	email VARCHAR(100)NOT NULL UNIQUE,
+	email VARCHAR(100)NOT NULL,
 	phone_number VARCHAR(20) NOT NULL,
 
 	-- Restricciones de validacion (CONSTRAINT)
+	CONSTRAINT pk_persons PRIMARY KEY (id),
 	-- Asegura que el email se unico
 	CONSTRAINT uq_person_email UNIQUE (email),
 	-- Asegura la estructura del email
 	CONSTRAINT chk_person_email_format CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+	-- Asegura la estructura del telefono
+	CONSTRAINT chk_phone CHECK (phone_number  ~* '^[+]?[0-9]{9,12}$'),
 	-- Asegura que los nombres no sean vacios
 	CONSTRAINT chk_full_name_not_empty CHECK (TRIM(full_name) <> '')
 );
 
 CREATE TABLE species ( -- Tabla de las especies
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-	common_name VARCHAR(100) NOT NULL
+	id INT GENERATED ALWAYS AS IDENTITY, 
+	common_name VARCHAR(100) NOT NULL,
+
+	-- Restricciones de validacion (CONSTRAINT)
+	CONSTRAINT pk_species PRIMARY KEY (id), --PK especies
+	-- Asegura que el nombre de la especie sea unico
+	CONSTRAINT uq_specie_name UNIQUE (common_name)
 );
 
-CREATE TABLE breed( -- Tabla de razas
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE breeds( -- Tabla de razas
+	id INT GENERATED ALWAYS AS IDENTITY,
 	common_name VARCHAR(100) NOT NULL,
-	id_species INT NOT NULL, -- FK de las especies
-
-	CONSTRAINT fk_breed_species
-		FOREIGN KEY (id_species) 
+	species_id INT NOT NULL, -- FK de las especies
+	
+	-- Restricciones de validacion (CONSTRAINT)
+	CONSTRAINT pk_breed PRIMARY KEY (id), -- pk de razas
+	CONSTRAINT uq_breed_name_species_id UNIQUE (common_name, species_id),
+	
+	CONSTRAINT fk_breed_specie
+		FOREIGN KEY (species_id) 
 		REFERENCES species (id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE
@@ -43,60 +54,48 @@ CREATE TABLE breed( -- Tabla de razas
 CREATE TYPE pet_gender AS ENUM ('M', 'F');
 
 CREATE TABLE pet_patients(
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-
+	id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(100) NOT NULL,
-	date_birth DATE,
+	date_birth DATE NOT NULL,
 	gender pet_gender NOT NULL,
-	color VARCHAR(50),
-	is_neutered BOOLEAN,
-	is_deceased BOOLEAN,
-	id_breed INT NOT NULL, -- FK DE RAZA
-	id_owner UUID NOT NULL, -- FK de persona
+	color VARCHAR(50) NOT NULL,
+	is_neutered BOOLEAN DEFAULT FALSE,
+	is_deceased BOOLEAN DEFAULT FALSE,
+	breed_id INT NOT NULL, -- FK DE RAZA
+	owner_id UUID NOT NULL, -- FK de persona
 
+	CONSTRAINT pk_pet_patients PRIMARY KEY (id),
+	
 	CONSTRAINT fk_pet_breed
-		FOREIGN KEY (id_breed) 
-		REFERENCES breed (id)
+		FOREIGN KEY (breed_id) 
+		REFERENCES breeds (id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE,
 
 	CONSTRAINT fk_pet_owner
-		FOREIGN KEY (id_owner) 
+		FOREIGN KEY (owner_id) 
 		REFERENCES persons (id)
-		ON DELETE RESTRICT
-		ON UPDATE CASCADE
-);
-
-CREATE TYPE appointment_type_enum AS ENUM ('Programada', 'Emergencia');
-			
-CREATE TABLE appointmen_type( -- Tabla de tipo de consulta 
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	name appointment_type_enum NOT NULL
-);
-
-CREATE TYPE appointment_status_enum AS ENUM ('Agendada', 'Cancelado', 'Ausente', 'Completa', 'En Curso');
-
-CREATE TABLE appointment_status( -- Tabla de estado de la consulta
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	name appointment_status_enum NOT NULL
-);
-
-CREATE TABLE specialties( -- Tabla de especialidades del veterinario
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	name VARCHAR (50)NOT NULL
-);
-
-CREATE TABLE veterinarians( -- Tabla de veterinario
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	person_id UUID NOT NULL, --FK Persona
-	specialties_id INT NOT NULL, -- Fk especialidad
-	
-	CONSTRAINT fk_veterinarians_specialties
-		FOREIGN KEY (specialties_id) 
-		REFERENCES specialties (id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE,
 
+	CONSTRAINT chk_date_birth CHECK (date_birth <= CURRENT_DATE)
+);
+
+CREATE TABLE specialties( -- Tabla de especialidades del veterinario
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR (50)NOT NULL,
+
+	CONSTRAINT uq_specialties_name UNIQUE (name),
+	CONSTRAINT pk_specialty PRIMARY KEY (id)
+);
+
+CREATE TABLE veterinarians( -- Tabla de veterinario
+	id INT GENERATED ALWAYS AS IDENTITY,
+	person_id UUID NOT NULL, --FK Persona
+
+	CONSTRAINT uq_veterinarian_person UNIQUE (person_id),
+	CONSTRAINT pk_veterinarian PRIMARY KEY (id),
+	
 	CONSTRAINT fk_veterinarians_person
 		FOREIGN KEY (person_id) 
 		REFERENCES persons (id)
@@ -104,19 +103,46 @@ CREATE TABLE veterinarians( -- Tabla de veterinario
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE appointment( -- Tabla de Cita 
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+CREATE TABLE veterinarians_specialties ( -- Tabla Intermedia paciete alergia
+	veterinarian_id INT,
+	specialty_id INT,
+
+	CONSTRAINT pk_veterinarians_specialties -- PK compuesta
+		PRIMARY KEY (veterinarian_id, specialty_id),
+
+	CONSTRAINT fk_veterinarians_specialties_specialtie
+		FOREIGN KEY (veterinarian_id) 
+		REFERENCES veterinarians (id)
+		ON DELETE RESTRICT,
+		
+	CONSTRAINT fk_veterinarians_specialties_specialty
+		FOREIGN KEY (specialty_id) 
+		REFERENCES specialties (id)
+		ON DELETE RESTRICT
+);
+
+CREATE TYPE appointment_type_enum AS ENUM ('Programada', 'Emergencia');
+CREATE TYPE appointment_status_enum AS ENUM ('Agendada', 'Cancelado', 'Ausente', 'Completa', 'En Curso');
+
+CREATE TABLE appointments( -- Tabla de Cita 
+	id INT GENERATED ALWAYS AS IDENTITY,
 	
-	date_at DATE,
+	scheduled_at TIMESTAMP NOT NULL,
+	ended_at  TIMESTAMP,
 	reason VARCHAR(150),
-	acompanying_id UUID NOT NULL, -- FK persona
+	accompanying_id UUID NOT NULL, -- FK persona
 	veterinarian_id INT NOT NULL, -- FK veterinario
 	pet_patient_id INT NOT NULL, -- Fk del paciente PETIENT
-	appointmen_type_id INT NOT NULL, -- FK tipo de cita
-	appointment_status_id INT NOT NULL, -- FK estado de cita
+	appointment_type appointment_type_enum NOT NULL, --tipo de cita
+	appointment_status appointment_status_enum NOT NULL, -- estado de cita
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- REGISTRO DE AUDITORIA
 
+    CONSTRAINT chk_ended_at CHECK  (ended_at IS NULL OR ended_at > scheduled_at), -- debe ser mayor a la fecha created_at
+	CONSTRAINT pk_appointment PRIMARY KEY (id),
+	
 	CONSTRAINT fk_appointment_person
-		FOREIGN KEY (acompanying_id) 
+		FOREIGN KEY (accompanying_id) 
 		REFERENCES persons (id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE,
@@ -131,37 +157,34 @@ CREATE TABLE appointment( -- Tabla de Cita
 		FOREIGN KEY (pet_patient_id) 
 		REFERENCES pet_patients (id)
 		ON DELETE RESTRICT
-		ON UPDATE CASCADE,
-
-	CONSTRAINT fk_appointment_type
-		FOREIGN KEY (appointmen_type_id) 
-		REFERENCES appointmen_type (id)
-		ON DELETE RESTRICT
-		ON UPDATE CASCADE,
-
-	CONSTRAINT fk_appointment_status_id
-		FOREIGN KEY (appointment_status_id) 
-		REFERENCES appointment_status (id)
-		ON DELETE RESTRICT
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE vaccine( -- Tabla de tipo de vacuna
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE vaccines( -- Tabla de tipo de vacuna
+	id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR (50)NOT NULL,
-	description VARCHAR(50)
+	description VARCHAR(255),
+
+	CONSTRAINT uq_vaccine_name UNIQUE (name),
+	CONSTRAINT pk_vaccine PRIMARY KEY (id)
 );
 
-CREATE TABLE medicine( -- Tabla de tipo de medicamentos
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE medicines( -- Tabla de tipo de medicamentos
+	id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR (50) NOT NULL,
-	description VARCHAR(50)
+	description VARCHAR(255),
+
+	CONSTRAINT uq_medicine_name UNIQUE (name),
+	CONSTRAINT pk_medicine PRIMARY KEY (id)
 );
 
 CREATE TABLE allergens( -- Tabla de tipo de alergenos
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR (50) NOT NULL,
-	description VARCHAR(50)
+	description VARCHAR(255),
+
+	CONSTRAINT uq_allergen_name UNIQUE (name),
+	CONSTRAINT pk_allergen PRIMARY KEY (id)
 );
 
 CREATE TABLE medicine_allergens (
@@ -173,7 +196,7 @@ CREATE TABLE medicine_allergens (
 
 	CONSTRAINT fk_medicine_allergens_medicine
 		FOREIGN KEY (medicine_id) 
-		REFERENCES medicine (id)
+		REFERENCES medicines (id)
 		ON DELETE RESTRICT,
 		
 	CONSTRAINT fk_medicine_allergens_allergens
@@ -183,7 +206,7 @@ CREATE TABLE medicine_allergens (
 	
 );
 
-CREATE TABLE patient_allergy ( -- Tabla Intermedia paciete alergia
+CREATE TABLE patient_allergies ( -- Tabla Intermedia paciete alergia
 	patient_id INT,
 	allergen_id INT,
 
@@ -203,25 +226,25 @@ CREATE TABLE patient_allergy ( -- Tabla Intermedia paciete alergia
 
 CREATE TABLE vaccine_species ( -- tabla intermedia vacuna especies
 	vaccine_id INT,
-	specie_id INT,
+	species_id INT,
 	
 	CONSTRAINT pk_vaccine_specie -- PK compuesta
-		PRIMARY KEY (vaccine_id, specie_id),
+		PRIMARY KEY (vaccine_id, species_id),
 
 	CONSTRAINT fk_vaccine_specie_vaccine
 		FOREIGN KEY (vaccine_id) 
-		REFERENCES vaccine(id)
+		REFERENCES vaccines(id)
 		ON DELETE RESTRICT,
 		
 	CONSTRAINT fk_vaccine_specie_species
-		FOREIGN KEY (specie_id) 
+		FOREIGN KEY (species_id) 
 		REFERENCES species (id)
 		ON DELETE RESTRICT
 );
 
 CREATE TABLE medical_history( -- Tabla de historial medico
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	date_appointment TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Fecha de registro
+	id INT GENERATED ALWAYS AS IDENTITY,
+	date_appointment TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Fecha de registro
 
 	--Informacion de la antencion
 	attention_description VARCHAR(200),
@@ -230,55 +253,63 @@ CREATE TABLE medical_history( -- Tabla de historial medico
 	weight REAL NOT NULL,
 	temperature REAL NOT NULL,
 	preassure VARCHAR (10),
-
+	
 	appointment_id INT,
+	
+	CONSTRAINT uq_medical_history_appointment UNIQUE (appointment_id),
 
-	--CONSTRAINT pk_medical_history -- PK del historial medico
-	--	PRIMARY KEY (id),
+	CONSTRAINT pk_medical_history -- PK del historial medico
+	PRIMARY KEY (id),
 
 	CONSTRAINT fk_medical_history_appointment
 		FOREIGN KEY (appointment_id) 
-		REFERENCES appointment(id)
+		REFERENCES appointments(id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE
 );
 
 CREATE TABLE vaccination_records( -- Tabla de historial de vacuna
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	date_record TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Fecha de registro
+	id INT GENERATED ALWAYS AS IDENTITY,
+	date_record TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Fecha de registro
 
 	--Informacion de la antencion
 	attention_description VARCHAR(200),
 	
 	appointment_id INT,
 
-	-- CONSTRAINT pk_vaccination_records -- PK del historial de vacuna
-	--	PRIMARY KEY (id),
+	CONSTRAINT uq_vaccination_records_appointment UNIQUE (appointment_id),
+	
+	CONSTRAINT pk_vaccination_records -- PK del historial de vacuna
+	PRIMARY KEY (id),
 
 	CONSTRAINT fk_vaccination_records_appointment
 		FOREIGN KEY (appointment_id) 
-		REFERENCES appointment(id)
+		REFERENCES appointments(id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE prescriptions(
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	date_record TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE prescriptions( -- TABLA DE RECETA MEDICA
+	id INT GENERATED ALWAYS AS IDENTITY,
+	date_record TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
 	--Informacion de la antencion
 	attention_description VARCHAR(200),
 	
-	appointment_id INT,
+	medical_history_id INT,
 
-	CONSTRAINT fk_prescriptions_appointment
-		FOREIGN KEY (appointment_id) 
-		REFERENCES appointment(id)
+	CONSTRAINT uq_prescriptions_medical_history UNIQUE (medical_history_id),
+	
+	CONSTRAINT pk_prescriptions PRIMARY KEY (id),-- PK del TABLA DE RECETA MEDICA
+	
+	CONSTRAINT fk_prescriptions_medical_history
+		FOREIGN KEY (medical_history_id) 
+		REFERENCES medical_history(id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE prescription_details(
+CREATE TABLE prescription_details( -- DETALLE DE RECETA MEDICA
 	prescriptions_id INT,
 	medicine_id INT,
 
@@ -292,7 +323,7 @@ CREATE TABLE prescription_details(
 
 	CONSTRAINT fk_prescription_details_medicine
 		FOREIGN KEY (medicine_id) 
-		REFERENCES medicine (id)
+		REFERENCES medicines (id)
 		ON DELETE RESTRICT
 );
 
